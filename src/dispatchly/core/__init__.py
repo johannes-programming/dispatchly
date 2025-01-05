2b38cf1131e8554586bd6c5783136376ceaa03e3
Johannes <johannes-programming@mailfence.com>
2025-01-05 17:48:34 +0100

    Version 0.0.2.dev0

--- src/dispatchly/core/__init__.py
+++ src/dispatchly/core/__init__.py
@@ -1,5 +1,6 @@
 import dataclasses
 import functools
+import types
 from typing import *
 
 import tofunc
@@ -15,38 +16,49 @@ def singledispatch(old, /):
     return Data(old).ans
 
 
+@dataclasses.dataclass(frozen=True)
+class Unpack:
+    kind: Any
+    func: Any
+
+    @classmethod
+    def byValue(cls, value: Any):
+        try:
+            func = value.__func__
+        except AttributeError:
+            func = value
+            kind = identity
+        else:
+            kind = type(value)
+        return cls(kind=kind, func=func)
+
+
 class Data:
-    def __init__(self, old):
+    def __init__(self, old: Any) -> None:
         self.ans = self.makeans(old)
 
-    def ans_1(self, arg, /, *args, **kwargs):
+    def ans_1(self, arg: Any, /, *args: Any, **kwargs: Any) -> Any:
         variant = self.getvariant(arg)
         return variant(arg, *args, **kwargs)
 
-    def getvariant(self, arg):
+    def getvariant(self, arg: Any) -> Any:
         for key, value in self.ans.registry.items():
             if isinstance(arg, key):
                 return value
         return self.ans.default
 
-    def makeans(self, old):
-        try:
-            default = old.__func__
-        except AttributeError:
-            default = old
-            bind = identity
-        else:
-            bind = type(old)
+    def makeans(self, old: Any) -> Any:
+        unpack = Unpack.byValue(old)
         ans = tofunc.tofunc(self.ans_1)
-        functools.wraps(default)(ans)
-        ans = bind(ans)
+        functools.wraps(unpack.func)(ans)
+        ans = unpack.kind(ans)
         ans._data = self
-        ans.default = default
+        ans.default = unpack.func
         ans.registry = dict()
         ans.register = self.makeregister()
         return ans
 
-    def makeregister(self):
+    def makeregister(self) -> types.FunctionType:
         def register(key: Any):
             return Register(ans=self.ans, key=key)
 
@@ -58,6 +70,6 @@ class Register:
     ans: Any
     key: Any
 
-    def __call__(self, value):
-        self.ans.registry[self.key] = value
+    def __call__(self, value: Any) -> Any:
+        self.ans.registry[self.key] = Unpack.byValue(value).func
         return self.ans
